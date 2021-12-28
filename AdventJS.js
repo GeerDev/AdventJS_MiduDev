c177bd7fbdc7a4e736c8734ec4ad73f7763da36f
Gerahara <gerfersie@gmail.com>
2021-12-28 20:44:26 +0100

    2º Commit AdventJS

--- AdventJS.js
+++ AdventJS.js
@@ -7,7 +7,7 @@ const ovejas = [
   ]
 
 function contarOvejas(ovejas) {
-    const resultado = ovejas.filter(element => element.color == 'rojo' && element.name.toLowerCase().includes('n') && element.name.toLowerCase().includes('a'));
+    const resultado = ovejas.filter(e => e.color == 'rojo' && e.name.toLowerCase().includes('n') && e.name.toLowerCase().includes('a'));
     return resultado
 }
   
@@ -15,9 +15,20 @@ const ovejasFiltradas = contarOvejas(ovejas)
 
 console.log(ovejasFiltradas)
 
-// Adviento 2
-var a;
-console.log(a)
+// Adviento 20
+function pangram(letter) {
+   const abecedario = 'abcdefghijklmnñopqrstuvwxyz'
+   const tranformacion = letter.toLowerCase();
+   for (let i = 0; i < abecedario.length; i++){
+     if (!tranformacion.includes(abecedario[i])){
+       return false;
+     }
+   }
+   return true;
+ }
 
+console.log(pangram('Extraño pan de col y kiwi se quemó bajo fugaz vaho')) // true
+console.log(pangram('Jovencillo emponzoñado y con walkman: ¡qué figurota exhibes!')) // true
 
-console.log(b)
+console.log(pangram('Esto es una frase larga pero no tiene todas las letras del abecedario')) // false
+console.log(pangram('De la a a la z, nos faltan letras')) // false
